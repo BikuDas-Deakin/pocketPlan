702418e9afbb3cf03e8b0833f30a4e326f4e3079
Bisheshwar-Das <141819955+Bisheshwar-Das@users.noreply.github.com>
2025-09-29 17:48:48 +1000

    chenges to server

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 3000;
 // ===== Middleware =====
 app.use(cors());
 app.use(express.json());
-app.use(express.static('public'));
+// app.use(express.static('public'));
 
 // ===== Database =====
 const pool = new Pool({
